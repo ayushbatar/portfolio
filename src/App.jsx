2f9aaac5e1893dc563b8f5fb53145672a2e3054d
Aayush Choudhary <aayushchoudhary519@gmail.com>
2024-06-14 18:21:34 +0000

    update the variable

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aayush chaudhary",
+  title: "Web Developer",
+  email: "Aayushcareer05@gmail.com",
+  gitHub: "ayushbatar",
+  instagram: "ayushgurzar",
+  linkedIn: "aayushbatar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#39CCCC";
+const secondaryColor = "#FF851B";
 
 const App = () => {
   return (
